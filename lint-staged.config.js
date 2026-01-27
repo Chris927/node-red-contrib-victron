8627cca13b41b78b71f91113f7c452a55d1627d3
Chris Oloff <chris@uber5.com>
2026-01-27 22:48:21 +0200

    lint-staged, 'npm run build' arguments issue, try js based config instead

---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [() => 'npm run build', () => 'npm run build:test', 'standard --fix', () => 'npm test'],
+  'src/nodes/*.html': [() => 'npm run documentation']
+}
