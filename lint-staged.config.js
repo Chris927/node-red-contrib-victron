64db86f2c115571065fac4033c9224265c8e226a
Chris Oloff <chris@uber5.com>
2026-01-27 22:54:12 +0200

    lint-staged, need to 'git add' possibly changed files in ./resources

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [() => 'npm run build', () => 'npm run build:test', 'standard --fix', () => 'npm test'],
+  '*.{js,jsx,ts,tsx}': [
+    () => 'npm run build',
+    () => 'npm run build:test',
+    'standard --fix',
+    () => 'git add', // we need this to add possibly changed files in ./resources (through build scripts)
+    () => 'npm test'
+  ],
   'src/nodes/*.html': [() => 'npm run documentation']
 }
