75dbaac07b0c9ae30ab9597207d89ad65ab6d465
Chris Oloff <chris@uber5.com>
2026-01-28 07:25:44 +0200

    lint-staged, remove unnecessary / incorrect 'git add' from lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,6 @@ module.exports = {
     () => 'npm run build',
     () => 'npm run build:test',
     'standard --fix',
-    () => 'git add', // we need this to add possibly changed files in ./resources (through build scripts)
     () => 'npm test'
   ],
   'src/nodes/*.html': [() => 'npm run documentation']
