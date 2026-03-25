5d8857c2af77be76db59f138611dcfbadfb70021
Chris Oloff <chris@uber5.com>
2026-03-25 21:53:10 +0200

    remove introspection call experimentally
    We have console.log() statements, which should be removed, or replaced with
    debug() statements; they are useful for now for manual testing, though.

--- src/services/dbus-listener.js
+++ src/services/dbus-listener.js
@@ -199,56 +199,30 @@ class VictronDbusListener {
 
     console.log(`Initializing service ${name} with owner ${owner}`)
 
-    // check if service supports GetValue on /DeviceInstance, which is required to
-    // get the device instance number.
-    const isGetValueSupported = await new Promise((resolve) => {
+    const deviceInstance = await new Promise((resolve) => {
       this.bus.invoke({
         path: '/DeviceInstance',
         destination: name,
-        interface: 'org.freedesktop.DBus.Introspectable',
-        member: 'Introspect'
+        interface: 'com.victronenergy.BusItem',
+        member: 'GetValue'
       },
       (err, res) => {
+        this.services[owner] = service
         if (err) {
-          console.warn(`Introspect failed for ${name}, cannot determine if GetValue is supported: ${err}`)
-          return resolve(false)
+          console.log(`initService ${name}, invoke on path /DeviceInstance : ${err}`)
         }
-        const xml = res
-        const hasGetValue = xml.includes('<method name="GetValue"')
-        console.log(`Service ${name} supports GetValue on /DeviceInstance: ${hasGetValue}`)
-        resolve(hasGetValue)
+        if (res) {
+          const deviceInstance = res[1]?.[0]
+          if (deviceInstance === undefined) {
+            console.error(`deviceInstance could not be assigned because res[1][0] is undefined owner=${owner} services[owner]=${JSON.stringify(this.services[owner])} (${this.services[owner].name})`)
+          }
+          return resolve(deviceInstance)
+        }
+        return resolve(null)
       })
     })
 
-    if (isGetValueSupported) {
-      const deviceInstance = await new Promise((resolve) => {
-        this.bus.invoke({
-          path: '/DeviceInstance',
-          destination: name,
-          interface: 'com.victronenergy.BusItem',
-          member: 'GetValue'
-        },
-        (err, res) => {
-          this.services[owner] = service
-          if (res) {
-            const deviceInstance = res[1]?.[0]
-            // TODO: instead of (!...) we now use (... === undefined), which allows for deviceInstance === 0 without error log output
-            // note that this does not change semantics, other than the error log output
-            if (deviceInstance === undefined) {
-              console.error(`deviceInstance could not be assigned because res[1][0] is undefined owner=${owner} services[owner]=${JSON.stringify(this.services[owner])} (${this.services[owner].name})`)
-              // TODO: Handle the case where res[1][0] is undefined
-            }
-            return resolve(deviceInstance)
-          }
-          if (err && err.length > 0) {
-            console.log(`initService ${name}, invoke on path /DeviceInstance : ${err}`)
-          }
-          return resolve(null)
-        })
-      })
-
-      this.services[owner].deviceInstance = deviceInstance
-    }
+    this.services[owner].deviceInstance = deviceInstance
 
     this._requestRoot(service)
   }
